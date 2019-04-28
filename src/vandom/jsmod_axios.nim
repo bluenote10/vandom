ea3eaf40f755140019d2529b1bfd82fc86ee2e70
Fabian Keller <github.100.fkeller@spamgourmet.com>
2019-04-28 19:22:38 +0200

    minor cleanup

--- src/vandom/jsmod_axios.nim
+++ src/vandom/jsmod_axios.nim
@@ -7,9 +7,7 @@ type
 when defined(nodejs):
   var axios* = require("axios", Axios)
 else:
-  #var axios* {.importc: "axios", nodecl.} : Axios
   var axios* = requireBrowser("axios", Axios)
-  debug("Axios:", axios)
 validateModule(axios)
 
 proc catch*(obj: JsObject, f: proc(obj: JsObject)): JsObject {.importcpp, nodecl.}
